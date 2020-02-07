40e4282934cb24b1daf963dc652f5a3bd2448227
Your Name <978190049@qq.com>
2020-02-07 21:40:27 +0800

    登录模块 - 标题区域布局

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="login-container">
-    <div class="left-box"></div>
+    <div class="left-box">
+      <div class="title-box">
+        <img src="../../assets/login-logo.png" alt class="logo" />
+        <span class="title">黑马面面</span>
+        <span class="line"></span>
+        <span class="sub-title">用户登录</span>
+      </div>
+    </div>
     <img src="../../assets/login_banner_ele.png" alt />
   </div>
 </template>
@@ -11,7 +18,7 @@ export default {
 };
 </script>
 
-<style>
+<style lang="less">
 .login-container {
   height: 100%;
   background: linear-gradient(
@@ -25,11 +32,44 @@ export default {
   align-items: center;
   /* 左右均分 */
   justify-content: space-around;
+
+  .left-box {
+    width: 478px;
+    height: 550px;
+    background-color: #f5f5f5;
+
+    .title-box {
+      display: flex;
+      align-items: center;
+      margin-left: 48px;
+      margin-top: 44px;
+      .logo {
+      }
+      .title {
+        font-size: 24px;
+        font-family: SourceHanSansCN;
+        color: rgba(12, 12, 12, 1);
+        margin-left: 16px;
+      }
+      .line {
+        width: 1px;
+        height: 28px;
+        background: rgba(199, 199, 199, 1);
+        margin-left: 14px;
+      }
+      .sub-title {
+        font-size: 22px;
+        font-family: PingFangSC;
+        color: rgba(12, 12, 12, 1);
+        margin-left: 12px;
+      }
+    }
+  }
 }
 
 .login-container .left-box {
   width: 478px;
   height: 550px;
-  background-color: #F5F5F5;
+  background-color: #f5f5f5;
 }
 </style>
