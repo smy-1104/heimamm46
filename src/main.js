eca6ad7628b0fe7dc7f3a12a472e459bc721c7cd
Your Name <978190049@qq.com>
2020-02-07 17:08:18 +0800

    项目起步 - 整合路由

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//导入路由
+import router from "./router/index.js"
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  //挂载到VUE实例上 路由
+  router
 }).$mount('#app')
