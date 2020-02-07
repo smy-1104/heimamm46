44540ce484d129507fd27cfba4b97bd3a4d254a9
Your Name <978190049@qq.com>
2020-02-07 20:57:45 +0800

    登录模块 - 整体布局

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,15 +1,35 @@
 <template>
-<div>
-我是一个登录页面
-</div>
+  <div class="login-container">
+    <div class="left-box"></div>
+    <img src="../../assets/login_banner_ele.png" alt />
+  </div>
 </template>
 <script>
 export default {
-    //组件名字
-    name:"login"
-}
+  //组件名字
+  name: "login"
+};
 </script>
 
 <style>
-    
+.login-container {
+  height: 100%;
+  background: linear-gradient(
+    225deg,
+    rgba(20, 147, 250, 1),
+    rgba(1, 198, 250, 1)
+  );
+  /*开启弹性布局*/
+  display: flex;
+  /* 上下居中 */
+  align-items: center;
+  /* 左右均分 */
+  justify-content: space-around;
+}
+
+.login-container .left-box {
+  width: 478px;
+  height: 550px;
+  background-color: #F5F5F5;
+}
 </style>
