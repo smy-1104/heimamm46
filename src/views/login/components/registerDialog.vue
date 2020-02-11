32b467c2ebc0e0871a99cae8a9e0f51c9942c6e8
Your Name <978190049@qq.com>
2020-02-11 14:54:47 +0800

    注册模块 - 文件上传组件整合

--- src/views/login/components/registerDialog.vue
+++ src/views/login/components/registerDialog.vue
@@ -7,6 +7,22 @@
     center
   >
     <el-form status-icon :model="form" :rules="rules" ref="registerForm">
+      <el-form-item label="头像">
+        <el-upload
+          class="avatar-uploader"
+          :action="uploadUrl"
+          :show-file-list="false"
+          :on-success="handleAvatarSuccess"
+          :before-upload="beforeAvatarUpload"
+          name="image"
+          v-model="form.image"
+        >
+          <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+          <!-- imgurl有值，显示图片 -->
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          <!-- imgurl没有有值，显示i标签 -->
+        </el-upload>
+      </el-form-item>
       <el-form-item label="昵称" prop="username" :label-width="formLabelWidth">
         <el-input v-model="form.username" autocomplete="off"></el-input>
       </el-form-item>
@@ -37,9 +53,10 @@
           </el-col>
           <el-col :span="7" :offset="1">
             <!-- 点击获取 短信验证码 -->
-            <el-button :disabled="delay != 0" @click="getSMS">
-               {{ delay == 0 ? '点击获取验证码' : `还有${delay}秒继续获取`}}
-            </el-button>
+            <el-button
+              :disabled="delay != 0"
+              @click="getSMS"
+            >{{ delay == 0 ? '点击获取验证码' : `还有${delay}秒继续获取`}}</el-button>
           </el-col>
         </el-row>
       </el-form-item>
@@ -52,6 +69,8 @@
 </template>
 
 <script>
+//导入
+// import  from ''
 
 //导入 axios
 import axios from "axios";
@@ -92,6 +111,8 @@ export default {
       dialogFormVisible: false,
       //表单数据
       form: {
+        //头像
+        image: "",
         //昵称
         username: "",
         //邮箱
@@ -105,6 +126,12 @@ export default {
       },
       //校验规则
       rules: {
+        //头像
+        image: [
+          {
+            required: true,         
+          }
+        ],
         username: [
           {
             required: true,
@@ -163,24 +190,48 @@ export default {
       //本地图片预览地址
       imageUrl: "",
       //头像上传的接口地址
-      uploadUrl: process.env.VUE_APP_URL+ "/uploads"
+      uploadUrl: process.env.VUE_APP_URL + "/uploads"
     };
   },
   methods: {
+    // 上传成功回调函数
+    // file 文件的各种信息
+    // file.raw 文件对象
+    // res是接口响应的数据
+    handleAvatarSuccess(res, file) {
+      window.console.log(res);
+      this.imageUrl = URL.createObjectURL(file.raw);
+      // URL.createObjectURL 生成的是本地的临时路径，刷新就没用了
+    },
+    // 上传之前
+    // file 是文件 对象
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === "image/jpeg" || "image/png";
+      const isLt2M = file.size / 1024 / 1024 < 2;
+
+      if (!isJPG) {
+        this.$message.error("上传头像图片只能是 JPG 格式!");
+      }
+      if (!isLt2M) {
+        this.$message.error("上传头像图片大小不能超过 2MB!");
+      }
+      return isJPG && isLt2M;
+    },
+
     //获取短信验证码
     getSMS() {
-        //如果为0开启倒计时
-        if (this.delay == 0){
-          this.delay = 60
-          const interId = setInterval(() => {
-            //时间的递减
-            this.delay--;
-            if(this.delay==0){
-              //清除定时器
-              clearInterval(interId)
-            } 
-          }, 100);
-        }
+      //如果为0开启倒计时
+      if (this.delay == 0) {
+        this.delay = 60;
+        const interId = setInterval(() => {
+          //时间的递减
+          this.delay--;
+          if (this.delay == 0) {
+            //清除定时器
+            clearInterval(interId);
+          }
+        }, 100);
+      }
 
       axios({
         url: process.env.VUE_APP_URL + "/sendsms",
@@ -194,10 +245,10 @@ export default {
       }).then(res => {
         //成功回调
         window.console.log(res);
-        if (res.data.code===200) {
-          this.$message.success('验证码获取成功：'+res.data.data.captcha)
-        } else if(res.data.code===0) {
-          this.$message.error(res.data.message)
+        if (res.data.code === 200) {
+          this.$message.success("验证码获取成功：" + res.data.data.captcha);
+        } else if (res.data.code === 0) {
+          this.$message.error(res.data.message);
         }
       });
     },
@@ -224,6 +275,36 @@ export default {
   .register-code {
     height: 40.8px;
     width: 100%;
+  }
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+
+  .avatar-uploader {
+    text-align: center;
+  }
+
+  .avatar-uploader .el-upload:hover {
+    border-color: #409eff;
+  }
+
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
   }
 }
 </style>
