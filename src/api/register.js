d7251f90d8a8955d7993cbab17714b0bbbcfad8c
Your Name <978190049@qq.com>
2020-02-11 18:11:25 +0800

    注册模块 - 注册接口抽取（之前的bug解决，验证码头像正常，return没写，端口号不一致）

--- src/api/register.js
+++ src/api/register.js
@@ -4,8 +4,20 @@ import axios from "axios"
 
 // 把注册相关的接口调用 抽取为函数 并暴露 带名字的暴露
 export function sendsms( data ) {
-    axios({
+    return axios({
         url:process.env.VUE_APP_URL+"/sendsms",
+        method:'post',
+        //是否跨域携带cookie 默认是false
+        withCredentials: true,
+        data
+    });
+}
+
+
+// 把注册接口抽取为方法
+export function register( data ) {
+    return axios({
+        url:process.env.VUE_APP_URL+"/register",
         method:'post',
         //是否跨域携带cookie 默认是false
         withCredentials: true,
