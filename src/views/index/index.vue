ebcf826ead5c400539f1039ea30830775cd71d3a
Your Name <978190049@qq.com>
2020-02-19 23:58:18 +0800

    首页模块 - 顶部布局

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -2,7 +2,18 @@
   <!-- 挂载的元素 -->
   <div id="app">
     <el-container class="my-container">
-      <el-header class="my-header">Header</el-header>
+      <el-header class="my-header">
+        <div class="left">
+          <i class="el-icon-s-fold"></i>
+          <img src="../../assets/index_logo.png" alt />
+          <span>黑马面面</span>
+        </div>
+        <div class="right">
+          <img src="../../assets/logo.png" alt />
+          <span class="name">圆圆，你好</span>
+          <el-button type="primary">退出</el-button>
+        </div>
+      </el-header>
       <el-container>
         <el-aside class="my-aside" width="200px">Aside</el-aside>
         <el-main class="my-main">Main</el-main>
@@ -29,7 +40,43 @@ body {
       height: 100%;
 
       .my-header {
-        background-color: tomato;
+        height: 60px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        .left {
+          display: flex;
+          align-items: center;
+          .el-icon-s-fold {
+            font-size: 24px;
+            margin-right: 22px;
+          }
+          img {
+            margin-right: 11px;
+          }
+          span {
+            font-size: 22px;
+            font-family: Microsoft YaHei;
+            font-weight: bold;
+            color: rgba(73, 161, 255, 1);
+          }
+        }
+        .right {
+          display: flex;
+          align-items: center;
+
+          img{
+              display: inline-block;
+              width: 43px;
+              height: 43px;
+              border-radius: 50%;
+              margin-right: 9px;
+          }
+          span.name{
+              margin-right: 38px;
+              font-size: 14px;
+          }
+        }
       }
       .my-aside {
         background-color: rgb(234, 240, 181);
