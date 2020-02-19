5a9f863c960734ed20289d6e798d51b76a8c7a7f
Your Name <978190049@qq.com>
2020-02-20 00:08:55 +0800

    首页模块 - 接口抽取

---
+++ src/api/index.js
@@ -0,0 +1,41 @@
+//导入 axios
+import axios from "axios"
+//导入token工具函数
+import {getToken} from "../uitils/token.js"
+
+
+const indexRequest = axios.create({
+    //基地址
+    baseURL:process.env.VUE_APP_URL,
+    //是否跨域
+    withCredentials:true
+})
+
+// 把用户信息相关的接口调用 
+export function info( data ) {
+    return indexRequest({
+        url:"/info",
+        method:'get',
+        //是否跨域携带cookie 默认是false
+        // withCredentials: true,
+        //设置请求头
+        //headers是axios定义的设置方式
+        headers:{
+            token:getToken()
+        }
+    });
+}
+// 把用户退出相关的接口调用 
+export function logout( data ) {
+    return indexRequest({
+        url:"/logout",
+        method:'get',
+        //是否跨域携带cookie 默认是false
+        // withCredentials: true,
+        //设置请求头
+        //headers是axios定义的设置方式
+        headers:{
+            token:getToken()
+        }
+    });
+}
