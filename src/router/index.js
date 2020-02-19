f77b61ab4e5f9a1eabe1e35c19a026dfc3632673
Your Name <978190049@qq.com>
2020-02-19 23:21:14 +0800

    首页模块 - 整体布局

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,9 @@ Vue.use(VueRouter)
 
 //导入 组件 登录
 import login from '../views/login/login.vue'
+
+//导入 组件 首页
+import index from '../views/index/index.vue'
 //创建路由对象
 const router = new VueRouter({
     //路由规则
@@ -16,7 +19,12 @@ const router = new VueRouter({
         {
                 path:"/login",
                 component:login
-        }
+        },
+        //首页
+        {
+                path:"/index",
+                component:index
+        },
     ]
 })
 //暴露出去
---
+++ src/views/index/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <!-- 挂载的元素 -->
+  <div id="app">
+    <el-container class="my-container">
+      <el-header class="my-header">Header</el-header>
+      <el-container>
+        <el-aside class="my-aside" width="200px">Aside</el-aside>
+        <el-main class="my-main">Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less">
+html,
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+
+  #app {
+    height: 100%;
+
+    .my-container {
+      height: 100%;
+
+      .my-header {
+        background-color: tomato;
+      }
+      .my-aside {
+        background-color: rgb(234, 240, 181);
+      }
+      .my-main {
+        background-color: rgb(195, 186, 247);
+      }
+    }
+  }
+}
+</style>
